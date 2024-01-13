aaa021b58866501d0a9beff344f8b3a5d001daf9
Barath Elangovan <barath_elangovan@outlook.com>
2024-01-13 17:01:03 +0530

    Landing page FIx

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -12,74 +12,82 @@ const socket = io.connect("http://localhost:3001");
 export const Landing = () => {
   const [userName, setUserName] = useState("");
   const [roomId, setRoomId] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = (userName: string, value: string) => {
     if (userName !== "" && value !== "") {
       socket.emit("join_room", value, userName);
+      setShowChat(true);
     }
   };
   return (
-    <Card
-      sx={{
-        minWidth: 300,
-        display: "flex",
-        justifyContent: "center",
-        background: "#bdbdbd",
-      }}
-    >
-      <CardContent>
-        <div
-          style={{
+    <div>
+      {!showChat ? (
+        <Card
+          sx={{
+            minWidth: 300,
             display: "flex",
             justifyContent: "center",
-            marginBottom: "20px",
-            marginTop: "10px",
+            background: "#bdbdbd",
           }}
         >
-          <Typography>Join a Chat-War Room</Typography>
-        </div>
-        <div style={{ marginBottom: "20px" }}>
-          <span>Name: </span>
-          <br></br>
-          <TextField
-            size="small"
-            id="standard-basic"
-            helperText="Please enter Your Name"
-            type="text"
-            variant="standard"
-            onChange={(event) => {
-              setUserName(event.target.value);
-            }}
-          />
-        </div>
+          <CardContent>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                marginBottom: "20px",
+                marginTop: "10px",
+              }}
+            >
+              <Typography>Join a Chat-War Room</Typography>
+            </div>
+            <div style={{ marginBottom: "20px" }}>
+              <span>Name: </span>
+              <br></br>
+              <TextField
+                size="small"
+                id="standard-basic"
+                helperText="Please enter Your Name"
+                type="text"
+                variant="standard"
+                onChange={(event) => {
+                  setUserName(event.target.value);
+                }}
+              />
+            </div>
 
-        <div style={{ marginBottom: "20px" }}>
-          <span>Room-ID: </span>
-          <br></br>
-          <TextField
-            size="small"
-            id="standard-basic"
-            helperText="Please enter Room ID"
-            type="text"
-            variant="standard"
-            onChange={(event) => {
-              setRoomId(event.target.value);
-            }}
-          />
-        </div>
-        <CardActions style={{ display: "flex", justifyContent: "center" }}>
-          <Button
-            variant="contained"
-            color="success"
-            size="small"
-            onClick={() => {
-              joinRoom(userName, roomId);
-            }}
-          >
-            Join Room
-          </Button>
-        </CardActions>
-      </CardContent>
-    </Card>
+            <div style={{ marginBottom: "20px" }}>
+              <span>Room-ID: </span>
+              <br></br>
+              <TextField
+                size="small"
+                id="standard-basic"
+                helperText="Please enter Room ID"
+                type="text"
+                variant="standard"
+                onChange={(event) => {
+                  setRoomId(event.target.value);
+                }}
+              />
+            </div>
+            <CardActions style={{ display: "flex", justifyContent: "center" }}>
+              <Button
+                variant="contained"
+                color="success"
+                size="small"
+                onClick={() => {
+                  joinRoom(userName, roomId);
+                }}
+              >
+                Join Room
+              </Button>
+            </CardActions>
+          </CardContent>
+        </Card>
+      ) : (
+        <Chat socket={socket} userName={userName} roomId={roomId} />
+      )}
+    </div>
   );
 };
